367a5b6b07bb013f2a7e3508213b92fe252e94b9
Georgi Georgiev <georgi.g@codecomrades.com>
2022-06-13 11:52:14 +0300

    configure db.json

--- cc-intern-task/src/pages/Home/Home.tsx
+++ cc-intern-task/src/pages/Home/Home.tsx
@@ -2,7 +2,11 @@ import { useEffect } from 'react';
 
 export const Home = () => {
     useEffect(() => {
-        fetch('https://my-json-server.typicode.com/ggeorgiev4/CC-Intern-Task/posts')
+        fetch('https://my-json-server.typicode.com/ggeorgiev4/CC-Intern-Task/posts/1')
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+
+        fetch('https://my-json-server.typicode.com/ggeorgiev4/CC-Intern-Task/users/1')
             .then((response) => response.json())
             .then((json) => console.log(json));
     });
